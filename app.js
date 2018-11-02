f2db16796a9ab06101ae8fd9a0c6835ad385d99d
Student Loaner 2 <studentloaner2@Admins-MacBook-Pro.local>
2018-11-02 15:30:48 -0700

    Added mongodb model

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const express = require('express')
 const app = express()
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/rotten-potatoes')
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+})
+
 var exphbs = require('express-handlebars')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 // MOCK ARRAY
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II"},
-    { title: "Laaaame", movieTitle: "Titanic"}
-]
+//let reviews = [
+//    { title: "Great Review", movieTitle: "Batman II"},
+//    { title: "Laaaame", movieTitle: "Titanic"},
+//    { title: "Best Movie Ever!", movieTitle: "Hot Rod"}
+//]
 
 // INDEX
 app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews})
+    Review.find()
+        .then(reviews =>{
+            res.render('reviews-index', { reviews: reviews})
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
 app.listen(3000, () => {
